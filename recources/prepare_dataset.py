29f4b84ad9e858d9d03f583aa3a65f1966216c29
Soner Steiner <sonersteiner@gmail.com>
2023-10-09 16:43:08 +0200

    Update prepare_dataset.py

--- recources/prepare_dataset.py
+++ recources/prepare_dataset.py
@@ -1,1 +1,37 @@
+import os
+import tensorflow as tf
+
+filepath="shakespeare.txt"
+with open(filepath) as f:
+    shakes_text = f.read()
+
+print(shakes_text[100:180])
+"".join(sorted(set(shakes_text.lower())))
+
+text_vec_layer = tf.keras.layers.TextVectorization(split="character",
+                                                   standardize="lower")
+text_vec_layer.adapt([shakes_text])
+encoded = text_vec_layer([shakes_text])[0]
+
+encoded -= 2  # drop tokens 0 (pad) and 1 (unknown), which we will not use
+n_tokens = text_vec_layer.vocabulary_size() - 2  # number of distinct chars = 39
+dataset_size = len(encoded) 
+
+def to_dataset(sequence, length, shuffle=False, seed=None, batch_size=32):
+    ds = tf.data.Dataset.from_tensor_slices(sequence)
+    ds = ds.window(length + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda window_ds: window_ds.batch(length + 1))
+    if shuffle:
+        ds = ds.shuffle(100_000, seed=seed)
+    ds = ds.batch(batch_size)
+    return ds.map(lambda window: (window[:, :-1], window[:, 1:])).prefetch(1)
+    
+list(to_dataset(text_vec_layer(["To be"])[0], length=4))
+
+length = 100
+tf.random.set_seed(42)
+train_set = to_dataset(encoded[:1_000_000], length=length, shuffle=True,
+                       seed=42)
+valid_set = to_dataset(encoded[1_000_000:1_060_000], length=length)
+test_set = to_dataset(encoded[1_060_000:], length=length)
 
